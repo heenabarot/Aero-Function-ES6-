bff626b08a114793761b878c8b7293f5d6c1aa04
Heena Barot <109453374+heenabarot@users.noreply.github.com>
2024-05-19 12:19:23 +0530

    Add files via upload

---
+++ Aero.js
@@ -0,0 +1,19 @@
+let msg=()=>console.log("Function Called");
+msg();
+let greeting=(x)=>console.log("Ho My Nmae Is "+x);
+greeting("Heena");
+let sum=(x,y)=>x+y;
+var ans=sum(30,50);
+console.log("sum is "+ans);
+let age =5;
+let welcome=(age<18)?
+()=>console.log('Baby'):
+()=>console.log('Adult');
+welcome();
+let addition =(a,b)=>{
+    let result=a+b;
+    return result;
+
+}
+let result = addition(8, 9);
+console.log(result1);
